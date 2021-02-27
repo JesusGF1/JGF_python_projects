f7c3f2bf158ee77eb6098a8cda00106364aba902
JesusGF1 <jesusgzlezferrer@gmail.com>
2021-02-27 11:55:10 +0100

    K nearest neighbors classifier for a binary cancer dataset

---
+++ ML_applications/Knn_Classifier.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec  6 11:03:39 2020
+
+@author: jesus
+"""
+
+#%%
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import f1_score
+from sklearn.metrics import recall_score
+#%%
+#pandas wisconsin cancer database with all features selected
+cancer = pd.read_csv("datasets\cancer_wisconsin.csv")
+cancer = cancer.drop(labels = "id", axis = 1)
+#%%
+label = cancer[cancer.columns[0]]
+features = cancer.drop(labels = "diagnosis", axis = 1)
+features = features.drop(labels = "Unnamed: 32", axis = 1)
+scaler = MinMaxScaler()
+scaled_features = scaler.fit_transform(features)
+
+#%%
+X_train, X_test, Y_train, Y_test = train_test_split(scaled_features,label, stratify=label, test_size=0.30)
+
+#%%
+neigh = KNeighborsClassifier(n_neighbors=2)
+neigh.fit(X_train, Y_train)
+Y_pred = neigh.predict(X_test)
+
+
+#%%
+f1_malign = f1_score(Y_test, Y_pred, pos_label="M",average = "binary")
+print(f1_malign)
+f1_benign = f1_score(Y_test, Y_pred, pos_label="B",average = "binary")
+print(f1_benign)
+acc = accuracy_score(Y_test, Y_pred)
+print(acc)
+rec = recall_score(Y_test, Y_pred, pos_label="M")
+print(rec)
