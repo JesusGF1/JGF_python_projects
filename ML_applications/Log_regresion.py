cc1f29c788e13e07f4816d80139513265863a0c5
JesusGF1 <jesusgzlezferrer@gmail.com>
2021-02-27 11:43:04 +0100

    Added an score function

--- ML_applications/Log_regresion.py
+++ ML_applications/Log_regresion.py
@@ -10,7 +10,6 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.metrics import accuracy_score
-from sklearn.metrics import roc_auc_score
 from sklearn.metrics import f1_score
 from sklearn.metrics import recall_score
 #%%
@@ -42,4 +41,6 @@ f1_benign = f1_score(Y_test, Y_pred, pos_label="B",average = "binary")
 print(f1_benign)
 acc = accuracy_score(Y_test, Y_pred)
 print(acc)
+rec = recall_score(Y_test, Y_pred, pos_label="M")
+print(rec)
 
