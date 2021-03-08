a78f4c740c3e04ef642533acf2b3265831f73981
JesusGF1 <jesusgzlezferrer@gmail.com>
2021-03-08 21:04:08 +0100

    Understood the webpage format now I will start writting the script

--- Web_scraping/Scraping_bbal_ref.py
+++ Web_scraping/Scraping_bbal_ref.py
@@ -28,15 +28,27 @@ print(bs.find("div", {'id':'div_teams_active'}))
 tabla = pd.read_html(str(bs.find("div", {'id':'div_teams_active'})))[0] #Tabla con datos generales sobre los equipos
 
 #%% url of the teams
-print(bs.find("div", {'id':'div_teams_active'}).findAll("a"))
+#print(bs.find("div", {'id':'div_teams_active'}).findAll("a"))
+teams = bs.find("div", {'id':'div_teams_active'}).findAll("a")
 
-
+print([team.get("href") for team in teams])
 
 #%%
 urlteam = "https://www.basketball-reference.com/teams/ATL/"
 html = urlopen(urlteam)
 bs = BeautifulSoup(html, 'html.parser')
-print(bs.find("div", {'id':'div_ATL'}).findAll("a"))
-print(bs.find("div", {'id':'div_ATL'}))
+print(bs.find("div", {'id':'div_ATL'}).find("a").get("href")) #with this I find the last season
+
+
+#print(bs.find("div", {'id':'div_ATL'}))
+
+#%% scraping players webpages
+url = "https://www.basketball-reference.com/teams/ATL/2021.html"
+html = urlopen(url)
+bs = BeautifulSoup(html, 'html.parser')
+print(bs.find("table", {'id':'roster'}).findAll("td", {"data-stat":"player"}))
+players = bs.find("table", {'id':'roster'}).findAll("td", {"data-stat":"player"})
+print([player.find("a").get("href") for player in players])
+
 #%%
 print([tag.name for tag in bs.find_all()])
