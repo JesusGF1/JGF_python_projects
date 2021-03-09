9b6ad53c2e9f5574375f08d1c83268b8ceb5e5af
JesusGF1 <jesusgzlezferrer@gmail.com>
2021-03-10 00:15:05 +0100

    Managed to scrape the stats of the NBA actual players

--- Web_scraping/scraping_teams.py
+++ Web_scraping/scraping_teams.py
@@ -45,7 +45,56 @@ def scraping_actual_team_players(team_abbreviation):
             pass
     return pd.concat(team_players_list)
 
-Tabla_equipo = scraping_actual_team_players("CHA")
+Tabla_equipo = scraping_actual_team_players("LAL")
 
 
+def scraping_league_stats():
+    """Scraping the entirety of the teamplayers"""
+    url="https://www.basketball-reference.com/teams/"
+    html = urlopen(url)
+    bs = BeautifulSoup(html, 'html.parser')
+    teams = bs.find("div", {'id':'div_teams_active'}).findAll("a")
+    teams_url = [team.get("href") for team in teams]
+    team_players_list = []
+    for team in teams_url:
+        time.sleep(3)
+        teamurl = domain + team
+        print(teamurl) #fafaf
+        html = urlopen(teamurl)
+        bs = BeautifulSoup(html, 'html.parser')
+        div_team = "div_" + teamurl[-4:-1]
+        season = bs.find("div", {'id':div_team}).find("a").get("href") #tengo que cambiarlo para que este guay
+        #esto tiene la URL de la ultima season
+        seasonurl = domain + season
+        print(seasonurl)
+        html = urlopen(seasonurl)
+        bs = BeautifulSoup(html, 'html.parser')
+        players = bs.find("table", {'id':'roster'}).findAll("td", {"data-stat":"player"})
+        player_url_list = [player.find("a").get("href") for player in players]
+        for player in player_url_list:
+            player_url = domain + player
+            time.sleep(3)
+            print(player_url)
+            html = urlopen(player_url)
+            bs = BeautifulSoup(html, 'html.parser')
+            try:
+                tabla = pd.read_html(str(bs.find("div", {'id':'all_per_poss'})).replace("<!--", ""))[0]        
+                tabla["Player"] = bs.find("h1", {"itemprop" : "name"}).text.strip()
+                indice = tabla[tabla["Season"]=="Career"].index[0]
+                tabla = tabla[0:indice]
+                tabla = tabla.drop(axis= 1,columns = "Unnamed: 29")
+                #no me encuentra tablas para uno del college que es el darlina01
+                print(player_url)
+                team_players_list.append(tabla)
+            except:
+                pass
+    return pd.concat(team_players_list)
+            
+    
+    
+Tabla_NBA = scraping_league_stats()    
+    
+Tabla_NBA.to_csv("NBA.csv")
 #pd.concat(list)
+
+#string = "https://www.basketball-reference.com/teams/BOS/"
